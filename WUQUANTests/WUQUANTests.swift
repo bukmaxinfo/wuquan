7c775df498309cc6b4e6c1867310ea806b274d5b
BookName <hahaming1n@gmail.com>
2025-07-19 20:35:39 -0700

    Initial Commit

---
+++ WUQUANTests/WUQUANTests.swift
@@ -0,0 +1,17 @@
+//
+//  WUQUANTests.swift
+//  WUQUANTests
+//
+//  Created by shuming li on 7/19/25.
+//
+
+import Testing
+@testable import WUQUAN
+
+struct WUQUANTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
